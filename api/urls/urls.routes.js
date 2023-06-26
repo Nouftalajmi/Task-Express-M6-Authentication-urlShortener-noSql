faf57164ea1c8ab20ae2335a02126f95fa721480
nouftalajmi <alajminouf94@gmail.com>
2023-06-26 14:20:03 +0300

    edited

--- api/urls/urls.routes.js
+++ api/urls/urls.routes.js
@@ -7,6 +7,6 @@ const { shorten, redirect, deleteUrl } = require('./urls.controllers');
 
 router.post('/shorten/:userId', passport.authenticate("jwt", { session: false }), shorten);
 router.get('/:code', redirect);
-router.delete('/:code', deleteUrl);
+router.delete('/:code', passport.authenticate('jwt', { session: false }), deleteUrl);
 
 module.exports = router;
